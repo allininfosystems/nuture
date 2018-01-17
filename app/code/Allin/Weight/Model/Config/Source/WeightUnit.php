a87d52db8340f10a1a61961fdfec615e0bb5cd80
Amit <allin.akumar@gmail.com>
2018-01-17 18:57:00 +0530

    weight

---
+++ app/code/Allin/Weight/Model/Config/Source/WeightUnit.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Allin\Weight\Model\Config\Source;
+
+class WeightUnit extends \Magento\Directory\Model\Config\Source\WeightUnit
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function toOptionArray()
+    {
+        return [['value' => 'lbs', 'label' => __('lbs')], ['value' => 'kgs', 'label' => __('kgs')], ['value' => 'gms', 'label' => __('grams')]];
+    }
+}
---
+++ app/code/Allin/Weight/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Allin_Weight',
+    __DIR__
+);

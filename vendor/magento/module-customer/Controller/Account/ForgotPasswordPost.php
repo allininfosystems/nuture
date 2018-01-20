44e1ee1fc2c62461cae361c2765df1876b6baa01
Amit <allin.akumar@gmail.com>
2018-01-20 14:38:03 +0530

    change reset password

--- vendor/magento/module-customer/Controller/Account/ForgotPasswordPost.php
+++ vendor/magento/module-customer/Controller/Account/ForgotPasswordPost.php
@@ -100,7 +100,7 @@ class ForgotPasswordPost extends \Magento\Customer\Controller\AbstractAccount
     protected function getSuccessMessage($email)
     {
         return __(
-            'If there is an account associated with %1 you will receive an email with a link to reset your password.',
+            'We are sending you an email with instructions to reset your password. If you don’t receive an email check your junk folder',
             $this->escaper->escapeHtml($email)
         );
     }
